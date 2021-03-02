50db84d4ebd8b2dc5a4ca0f946515ff9dbed6973
melnikova daria <79516658127@ya.ru>
2021-03-02 13:35:54 +0300

    Add Tasks1-6 from day 1

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -3,5 +3,15 @@ package day1;
 public class Task6 {
     public static void main(String[] args) {
 
+        int k = 2;
+        int k1 = 1;
+        int sum = 0;
+        for (k1=1; k1<=9; k1++) {
+            sum = k1*k;
+
+            System.out.println(k1 + " x " + k + " = " + sum);
+
+        }
+
     }
 }
